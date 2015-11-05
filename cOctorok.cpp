1306250e21bd4627a0688ead19275a803ddbe684
albertnez <albertmg7@gmail.com>
2015-11-05 01:31:36 +0100

    Added octorok that moves around!

---
+++ cOctorok.cpp
@@ -0,0 +1,41 @@
+#include "cOctorok.h"
+
+const int cOctorok::width = 16;
+const int cOctorok::height = 16;
+const int cOctorok::step = 1;
+const std::vector<Direction> cOctorok::nextDir {
+	Direction::Left,   // From None to left.
+	Direction::Right,  // From Up to right.
+	Direction::Left,   // From Down to left.
+	Direction::Up,     // From Left to up.
+	Direction::Down,   // From Right to down.
+};
+
+
+cOctorok::cOctorok() : sceneX(-1), sceneY(-1) {
+	Init();
+}
+
+cOctorok::cOctorok(int x, int y, int sceneX, int sceneY) 
+	: cBicho(x, y, width, height),
+	sceneX(sceneX), sceneY(sceneY) {
+	Init();
+}
+
+void cOctorok::Logic(const cMap &map) {
+	if (!Move(map, direction, sceneX, sceneY)) {
+		direction = nextDir[int(direction)];
+		SetAnimation(to_string(direction));
+	}
+
+}
+
+void cOctorok::Init() {
+	animations = LoadAnimations("res/octorok.anim");
+	currentAnimation = "down";
+	w = width;
+	h = height;
+	stepLength = step;
+}
+
+
---
+++ cOctorok.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "cBicho.h"
+
+class cOctorok : public cBicho {
+public:
+	cOctorok();
+	cOctorok(int x, int y, int sceneX, int sceneY);
+
+	void Logic(const cMap &map) override;
+private:
+	static const int width;
+	static const int height;
+	static const int step;
+	static const std::vector<Direction> nextDir;
+
+	void Init();
+	// Which view of the scene is in.
+	int sceneX;
+	int sceneY;
+};
